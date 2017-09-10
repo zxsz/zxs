04adf1ca20823e80ef87a9e39780360220e4bcd8
zxslinux <root@localhost.localdomain>
2017-09-10 15:02:36 +0800

    2

--- checkdisk.sh
+++ checkdisk.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 #
-DEV_LIST=`df | sed -r '/^\/dev\/[sh]d/!d;s@(^/dev/[sh]d[a-z][0-9]{1,}).*([0-9]{2,3})%.*@\1:\2@'`
-for i in $DEV_LIST;do
-    [ ${i#*:} -ge 8 ] &&  wall "Warnning  $i% will full"
-done
-
-
+df | awk '/\/dev\/sd/{if(+$5>80)print $1,"will be full used:",$5}'
